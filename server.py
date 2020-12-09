09da37644d14142d4af86b09c1aa642fadb2d640
sharmita3 <49931235+sharmita3@users.noreply.github.com>
2020-12-09 13:01:06 -0500

    minor edit

--- server.py
+++ server.py
@@ -4,14 +4,12 @@ import socketserver
 
 from http import HTTPStatus
 
-
 class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
         self.end_headers()
         msg = 'Hello there!! you requested %s' % (self.path)
         self.wfile.write(msg.encode())
-
 
 port = int(os.getenv('PORT', 80))
 print('Listening on port %s' % (port))
